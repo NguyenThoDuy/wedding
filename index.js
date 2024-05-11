4fa132cbd90150d72c5f22be60c8282b4a678299
thoduyqt@gmail.com <thoduyqt@gmail.com>
2024-05-11 16:45:46 +0700

    add

--- index.js
+++ index.js
@@ -1,22 +1,15 @@
-
-
-window.addEventListener('load', (event) => {
-         // Sử dụng hàm với đường dẫn đến file JSON
-     const filePath = 'comments.json';
-     readCommentsFromFile();
-     });
-     
-   
-     function readCommentsFromFile() {
-         const xhr = new XMLHttpRequest();
-         xhr.overrideMimeType("application/json");
-         xhr.open('GET', 'comments.json', true);
-         xhr.onreadystatechange = function () {
-            if (xhr.readyState === 4 && xhr.status === 200) {
+function readCommentsFromFile() {
+    const xhr = new XMLHttpRequest();
+    xhr.overrideMimeType("application/json");
+    xhr.open('GET', 'comments.json', true);
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState === 4) {
+            if (xhr.status === 200) {
                 try {
                     const comments = JSON.parse(xhr.responseText);
                     const wishBox = document.querySelector('.wish-box');
                     comments.forEach(comment => {
+                        console.log(comment.id)
                         let commentHTML = '';
                         if (comment.id % 2 === 0) {
                             commentHTML = '<div class="wish-box-item bg"><strong>' + comment.name.replace(/&/g, "&amp;").replace(/>/g, "&gt;").replace(/</g, "&lt;").replace(/"/g, "&quot;") + '</strong><p>' + comment.content.replace(/&/g, "&amp;").replace(/>/g, "&gt;").replace(/</g, "&lt;").replace(/"/g, "&quot;") + '</p></div>';
@@ -28,12 +21,11 @@ window.addEventListener('load', (event) => {
                 } catch (error) {
                     console.error('Error parsing JSON:', error);
                 }
+            } else {
+                console.error('Failed to load comments. Status:', xhr.status);
             }
-        };
-        
-         xhr.send(null);
-     } 
-     
-     
-     
-     
+        }
+    };
+    
+    xhr.send();
+}
