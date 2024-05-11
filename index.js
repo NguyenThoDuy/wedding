3a65ddfd69c9546da2a6aa9438975b5c05d3cf97
thoduyqt@gmail.com <thoduyqt@gmail.com>
2024-05-11 16:42:08 +0700

    edit

--- index.js
+++ index.js
@@ -1,7 +1,4 @@
-/*https://github.com/phiphi1992/emoji-picker-element*/
-// import Picker from './picker.js'
-// import Database from './database.js'
-// export { Picker, Database }
+
 
 window.addEventListener('load', (event) => {
          // Sử dụng hàm với đường dẫn đến file JSON
@@ -15,32 +12,25 @@ window.addEventListener('load', (event) => {
          xhr.overrideMimeType("application/json");
          xhr.open('GET', 'comments.json', true);
          xhr.onreadystatechange = function () {
-             if (xhr.readyState === 4 && xhr.status === 200) {
-                 try {
-                     const comments = JSON.parse(xhr.responseText);
-                    //  comments.forEach(comment => {
-                    //      console.log('ID:', comment.id);
-                    //      console.log('Name:', comment.name);
-                    //      console.log('Content:', comment.content);
-                    //      console.log('---------------------');
-                    //  });
+            if (xhr.readyState === 4 && xhr.status === 200) {
+                try {
+                    const comments = JSON.parse(xhr.responseText);
                     const wishBox = document.querySelector('.wish-box');
                     comments.forEach(comment => {
-                        const commentHTML = '';
-                        if(comment.id % 2 == 0){
+                        let commentHTML = '';
+                        if (comment.id % 2 === 0) {
                             commentHTML = '<div class="wish-box-item bg"><strong>' + comment.name.replace(/&/g, "&amp;").replace(/>/g, "&gt;").replace(/</g, "&lt;").replace(/"/g, "&quot;") + '</strong><p>' + comment.content.replace(/&/g, "&amp;").replace(/>/g, "&gt;").replace(/</g, "&lt;").replace(/"/g, "&quot;") + '</p></div>';
-                        }else{
+                        } else {
                             commentHTML = '<div class="wish-box-item"><strong>' + comment.name.replace(/&/g, "&amp;").replace(/>/g, "&gt;").replace(/</g, "&lt;").replace(/"/g, "&quot;") + '</strong><p>' + comment.content.replace(/&/g, "&amp;").replace(/>/g, "&gt;").replace(/</g, "&lt;").replace(/"/g, "&quot;") + '</p></div>';
                         }
-                        
-                        
                         wishBox.insertAdjacentHTML('afterbegin', commentHTML);
                     });
-                 } catch (error) {
-                     console.error('Error parsing JSON:', error);
-                 }
-             }
-         };
+                } catch (error) {
+                    console.error('Error parsing JSON:', error);
+                }
+            }
+        };
+        
          xhr.send(null);
      } 
      
