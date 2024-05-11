9592915d6b12b4d374c82ff20edc3ef7f16639b0
DuyNguyen <80323466+NguyenThoDuy@users.noreply.github.com>
2024-05-11 15:48:23 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,37 @@
+/*https://github.com/phiphi1992/emoji-picker-element*/
+// import Picker from './picker.js'
+// import Database from './database.js'
+// export { Picker, Database }
+
+window.addEventListener('load', (event) => {
+         // Sử dụng hàm với đường dẫn đến file JSON
+     const filePath = 'comments.json';
+     readCommentsFromFile();
+     });
+     
+   
+     function readCommentsFromFile() {
+         const xhr = new XMLHttpRequest();
+         xhr.overrideMimeType("application/json");
+         xhr.open('GET', 'comments.json', true);
+         xhr.onreadystatechange = function () {
+             if (xhr.readyState === 4 && xhr.status === 200) {
+                 try {
+                     const comments = JSON.parse(xhr.responseText);
+                     comments.forEach(comment => {
+                         console.log('ID:', comment.id);
+                         console.log('Name:', comment.name);
+                         console.log('Content:', comment.content);
+                         console.log('---------------------');
+                     });
+                 } catch (error) {
+                     console.error('Error parsing JSON:', error);
+                 }
+             }
+         };
+         xhr.send(null);
+     } 
+     
+     
+     
+     
