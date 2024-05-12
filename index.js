a5f5915c76358c825d5d2c60a016f158a9cd79da
thoduyqt@gmail.com <thoduyqt@gmail.com>
2024-05-12 16:08:56 +0700

    add

--- index.js
+++ index.js
@@ -1,141 +1,192 @@
+// Xây dựng URL để gửi yêu cầu POST
+const url = 'https://script.google.com/macros/s/AKfycbxQDLN5O8iYe-W5X3J1YgYYRugiiS0hoUoeGgR22ysyZ1HOVHTXv-6gKy6b-A3Hh1fpfA/exec';
+
+
 window.addEventListener('load', (event) => {
-         // Sử dụng hàm với đường dẫn đến file JSON
-     const filePath = 'comments.json';
-     readCommentsFromFile();
-     });
-     
-     function readCommentsFromFile() {
-        const xhr = new XMLHttpRequest();
-        xhr.overrideMimeType("application/json");
-        xhr.open('GET', 'comments.json', true);
-        xhr.onreadystatechange = function () {
-            if (xhr.readyState === 4) {
-                if (xhr.status === 200) {
-                    try {
-                        const comments = JSON.parse(xhr.responseText);
-                        
-                        // Sắp xếp các comments theo trường id từ lớn đến bé
-                        comments.sort((a, b) => b.id - a.id);
-    
-                        const wishBox = document.querySelector('.wish-box');
-                        comments.forEach(comment => {
-                            let commentHTML = '';
-                            if (comment.id % 2 === 0) {
-                                commentHTML = '<div class="wish-box-item bg"><strong>' + comment.name.replace(/&/g, "&amp;").replace(/>/g, "&gt;").replace(/</g, "&lt;").replace(/"/g, "&quot;") + '</strong><p>' + comment.content.replace(/&/g, "&amp;").replace(/>/g, "&gt;").replace(/</g, "&lt;").replace(/"/g, "&quot;") + '</p></div>';
-                            } else {
-                                commentHTML = '<div class="wish-box-item"><strong>' + comment.name.replace(/&/g, "&amp;").replace(/>/g, "&gt;").replace(/</g, "&lt;").replace(/"/g, "&quot;") + '</strong><p>' + comment.content.replace(/&/g, "&amp;").replace(/>/g, "&gt;").replace(/</g, "&lt;").replace(/"/g, "&quot;") + '</p></div>';
-                            }
-                            wishBox.insertAdjacentHTML('afterbegin', commentHTML);
-                        });
-                    } catch (error) {
-                        console.error('Error parsing JSON:', error);
-                    }
-                } else {
-                    console.error('Failed to load comments. Status:', xhr.status);
-                }
-            }
-        };
-        
-        xhr.send();
-    }
-    
 
-    function addComment(name, content) {
-        const xhr = new XMLHttpRequest();
-        xhr.overrideMimeType("application/json");
-        xhr.open('GET', 'comments.json', true);
-        xhr.onreadystatechange = function () {
-            if (xhr.readyState === 4 && xhr.status === 200) {
-                try {
-                    let comments = JSON.parse(xhr.responseText);
-                    // Tìm id lớn nhất trong danh sách comments
-                    let maxId = 0;
-                    comments.forEach(comment => {
-                        if (comment.id > maxId) {
-                            maxId = comment.id;
-                        }
-                    });
-                    // Tăng giá trị của id lên 1 cho comment mới
-                    const newComment = {
-                        id: maxId + 1,
-                        name: name,
-                        content: content
-                    };
-                    // Thêm comment mới vào danh sách comments
-                    comments.push(newComment);
-                    
-                    // Ghi lại danh sách comments mới vào file JSON
-                    saveCommentsToFile(comments);
-                } catch (error) {
-                    console.error('Error parsing JSON:', error);
-                }
-            }
-        };
-        xhr.send(null);
+    getDataFromGoogleSheet();
+    // Lấy giá trị của tham số "name" từ URL
+    let nameValueUrl = new URLSearchParams(window.location.search).get('name');
+    let displayName = ''
+    if (nameValueUrl !== null && nameValueUrl.trim() !== '') {
+        displayName = nameValueUrl
+    }else{
+        displayName = 'Bạn'
     }
-    // https://script.google.com/macros/s/AKfycbxQDLN5O8iYe-W5X3J1YgYYRugiiS0hoUoeGgR22ysyZ1HOVHTXv-6gKy6b-A3Hh1fpfA/exec
-    function saveCommentsToFile(comments) {
-        const jsonString = JSON.stringify(comments);
-        // Sử dụng API của JavaScript để lưu chuỗi JSON vào file
-        // Trong trường hợp này, đường dẫn và cách lưu trữ có thể thay đổi tùy theo yêu cầu của ứng dụng của bạn
-        // Ví dụ: localStorage.setItem('comments', jsonString); để lưu vào localStorage
-        // Hoặc sử dụng Node.js hoặc các công cụ back-end khác để lưu vào file server-side
-        console.log('Comments saved successfully.');
-    }
-    
-    
-    document.addEventListener('DOMContentLoaded', function() {
-        const wishForm = document.getElementById('wish-form-add-comment');
-        if (wishForm) {
-            wishForm.addEventListener('submit', function(event) {
-                event.preventDefault(); // Ngăn chặn hành động mặc định của submit form
-    
-                // Lấy thông tin từ các trường input trong form
-                const name = document.getElementById('name-input').value;
-                const content = document.getElementById('content-input').value;
-
-                console.log("====> " + name);
-                console.log("====> " + content);
-
-// Sử dụng hàm để thêm comment mới
-                addCommentToGoogleSheet(name, content);
-    
-                // Optional: reset form sau khi submit
-                wishForm.reset();
-            });
+        const h2Element = document.querySelector('.section-title-h2');
+        if(h2Element !== null){
+            h2Element.innerText = 'Kính mời: ' + displayName
+            let vaitro = getStartName(displayName);
+            let text = '<i style="color: rebeccapurple;">Tới dự hôn lễ của ' + vaitro + '</i>.</br> Sự hiện diện của ' + nameValueUrl + ' là lời chúc phúc tuyệt vời nhất dành cho vợ chồng ' + vaitro;
+            const h2LoiMoiElement = document.querySelector('.section-title-loi-moi');
+            h2LoiMoiElement.innerHTML = text;
         }
-    });
+        if(displayName.length < 10){
+            document.getElementById('name-input').value = displayName; 
+        }
+    
+});
 
-    function addCommentToGoogleSheet(name, content) {
-        // Dữ liệu mới cần thêm vào
-        const newData = [name, content];
-    
-        // Xây dựng URL để gửi yêu cầu POST
-        const url = 'https://script.google.com/macros/s/AKfycbxQDLN5O8iYe-W5X3J1YgYYRugiiS0hoUoeGgR22ysyZ1HOVHTXv-6gKy6b-A3Hh1fpfA/exec';
-    
-        // Tạo yêu cầu XMLHttpRequest
-        const xhr = new XMLHttpRequest();
-        xhr.open('POST', url);
-        xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-        xhr.onreadystatechange = function() {
-            if (xhr.readyState === 4 && xhr.status === 200) {
-                console.log('Comment added successfully.');
-            } else {
-                console.error('Failed to add comment:', xhr.responseText);
+
+function getStartName(nameValue){
+    if (nameValue.startsWith('Bạn')){
+        return 'Chúng tớ';
+    }else if (nameValue.startsWith('Anh') || nameValue.startsWith('Chị')){
+        return 'Chúng em';
+    }else if (nameValue.startsWith('Chú') || nameValue.startsWith('Cô') || nameValue.startsWith('Bác') || nameValue.startsWith('Dì')){
+        return 'Chúng cháu';
+    }else if (nameValue.startsWith('Em')){
+        return 'Anh/Chị';
+    }else if (nameValue.startsWith('Cháu')){
+        return 'Cô/Chú';
+    }else if (nameValue.startsWith('Mẹ')){
+        return 'Chúng con';
+    }else{
+        return 'Chúng tôi'
+    }
+}
+
+function getDataFromGoogleSheet() {
+    fetch('https://script.google.com/macros/s/AKfycbySK00-BsTEIz-8Lk-MkRJpynUynSYzrcWaNNe3_rvhymZikscGd0PjQHDq510_bklxAg/exec')
+        .then(res => res.json())
+        .then(data => {
+            const rows = data.content;
+            const jsonArray = [];
+
+            rows.forEach((row, index) => {
+                if (index > 0) {
+                    const id = index;
+                    const name = row[0];
+                    const comment = row[1];
+
+                    const jsonItem = {
+                        "id": id,
+                        "name": name,
+                        "comment": comment
+                    };
+                    jsonArray.push(jsonItem);
+                }
+
+            });
+
+            console.log(jsonArray); // Log the JSON array
+            readCommentsFromFile(jsonArray);
+        })
+        .catch(error => console.error('Error fetching data:', error));
+}
+
+
+
+
+
+function readCommentsFromFile(comments) {
+    const wishBox = document.querySelector('.wish-box');
+    comments.forEach(comment => {
+        let commentHTML = '';
+        if (comment.id % 2 === 0) {
+            commentHTML = '<div class="wish-box-item bg"><strong>' + comment.name + '</strong><p>' + comment.comment + '</p></div>';
+        } else {
+            commentHTML = '<div class="wish-box-item"><strong>' + comment.name + '</strong><p>' + comment.comment + '</p></div>';
+        }
+        wishBox.insertAdjacentHTML('afterbegin', commentHTML);
+    });
+};
+
+
+
+// https://script.google.com/macros/s/AKfycbxQDLN5O8iYe-W5X3J1YgYYRugiiS0hoUoeGgR22ysyZ1HOVHTXv-6gKy6b-A3Hh1fpfA/exec
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    const wishForm = document.getElementById('wish-form-add-comment');
+    if (wishForm) {
+        wishForm.addEventListener('submit', function (event) {
+            event.preventDefault(); // Ngăn chặn hành động mặc định của submit form
+
+            // Lấy thông tin từ các trường input trong form
+            const name = document.getElementById('name-input').value;
+            const content = document.getElementById('content-input').value;
+            if (name === null || name === '' || content === null || content === '') {
+                // Lấy thẻ div theo id
+                const errorDiv = document.getElementById('error');
+                errorDiv.innerText = 'Họ tên & lời chúc vui lòng không để trống'
+                // Hiển thị thẻ
+                errorDiv.style.display = 'block';
+
+                // Sau 3 giây, ẩn thẻ
+                setTimeout(function () {
+                    errorDiv.style.display = 'none';
+                }, 3000);
+            }else{
+                // Sử dụng hàm để thêm comment mới
+            addCommentToGoogleSheet(name, content);
+
+            // Optional: reset form sau khi submit
+            wishForm.reset();
             }
-        };
-    
-        // Chuẩn bị dữ liệu để gửi
-        const params = 'name=' + encodeURIComponent(newData[0]) +
-                       '&content=' + encodeURIComponent(newData[1]);
-    
+        });
+    }
+});
+
+function addCommentToGoogleSheet(name, content) {
+    // Dữ liệu mới cần thêm vào
+    const newData = [name, content];
+
+
+    // Tạo yêu cầu XMLHttpRequest
+    const xhr = new XMLHttpRequest();
+    xhr.open('POST', url);
+    xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+            const commentCurrentArr = document.querySelectorAll('.wish-box-item');
+            let numberSize = commentCurrentArr.length
+            const wishBox = document.querySelector('.wish-box');
+            let commentHTML = ''
+            if (numberSize % 2 !== 0) {
+                commentHTML = '<div class="wish-box-item bg"><strong>' + name + '</strong><p>' + content + '</p></div>';
+            } else {
+                commentHTML = '<div class="wish-box-item"><strong>' + name + '</strong><p>' + content + '</p></div>';
+            }
+            wishBox.insertAdjacentHTML('afterbegin', commentHTML);
+        } else {
+            console.error('Failed to add comment:', xhr.responseText);
+        }
+    };
+
+    // Chuẩn bị dữ liệu để gửi
+    const params = 'name=' + encodeURIComponent(newData[0]) +
+        '&content=' + encodeURIComponent(newData[1]);
+    try {
         // Gửi yêu cầu POST với dữ liệu
         xhr.send(params);
+        // Lấy thẻ div theo id
+        const successDiv = document.getElementById('success');
+
+        // Hiển thị thẻ
+        successDiv.style.display = 'block';
+
+        // Sau 3 giây, ẩn thẻ
+        setTimeout(function () {
+            successDiv.style.display = 'none';
+        }, 3000);
+    } catch (error) {
+        // Lấy thẻ div theo id
+        const errorDiv = document.getElementById('error');
+        errorDiv.innerText = 'Gửi lời chúc thất bại, vui lòng thử lại sau giây lát!'
+        // Hiển thị thẻ
+        errorDiv.style.display = 'block';
+
+        // Sau 3 giây, ẩn thẻ
+        setTimeout(function () {
+            errorDiv.style.display = 'none';
+        }, 3000);
     }
-        
-    
-    
-     
-     
-     
-     
+
+}
+
+
+
+
+
+
